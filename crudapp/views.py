c0603b72f6aabf67540d4c8ebed9b0a70581249f
Laurah-O <okodelaura720@gmail.com>
2024-11-15 10:43:48 +0300

    Initial commit

---
+++ crudapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Item
+
+# Create your views here.
+
+#CRUD - Create
+
+def create_item(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        description = request.POST.get('description')
+        Item.objects.create(name=name, description=description)
+        return redirect('item_list')
+    return render(request, 'item_form.html')
+
+#CRUD - Read
+
+def item_list(request):
+    items=Item.objects.all()
+    return render(request, 'item_list.html', {'items': items})
+
+#CRUD - Update
+
+def update_item(request,pk):
+    item=get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.name=request.POST.get('name')
+        item.description = request.POST.get('description')
+        item.save()
+        return redirect('item_list')
+    return render(request, 'item_form.html', {'item': item})
+
+#CRUD - Delete
+
+def delete_item(request, pk):
+    item=get_object_or_404(Item, pk=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('item_list')
+    return render(request, 'item_confirm_delete.html', {'item': item})
+
